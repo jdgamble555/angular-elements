e64b3b0275b12a45877fcfb6db86097823398ee6
Jonathan Gamble <jdgamble555@gmail.com>
2023-09-29 21:08:02 -0500

    final working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,16 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const element = createCustomElement(AppComponent, {
+      injector: this.injector
+    });
+    customElements.define('app-micro', element);
+  }
+}
